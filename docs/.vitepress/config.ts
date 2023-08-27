64d5797075a2f5c49a0dcee24ea1045aba80b15c
NONPLAYT <kidzania177@gmail.com>
2023-08-28 00:37:35 +0300

    [ci-skip] full rework of docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  head: [['link', { rel: 'icon', href: 'assets/favicon.ico' }]],
+  title: "DivineMC",
+  description: "DivineMC is the fork of Purpur compatible with Spigot plugins, offering the best performance for your server.",
+  themeConfig: {
+    logo: 'assets/logo.png',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Documentation', link: '/docs/' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Documentation',
+        items: [
+          { text: 'Overview', link: '/docs/' }
+        ]
+      },
+      {
+        text: 'Administration',
+        items: [
+          { text: 'Getting Started', link: '/docs/admin/getting-started' },
+          { text: 'Configuration', link: '/docs/admin/configuration' }
+        ]
+      },
+      {
+        text: 'Development',
+        items: [
+          { text: 'Contributing to DivineMC', link: '/docs/dev/contributing' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/DivineMC/DivineMC' },
+      { icon: 'discord', link: 'https://discord.gg/p7cxhw7E2M' }
+    ]
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,17 @@
+---
+layout: home
+
+hero:
+  name: "DivineMC"
+  tagline: "DivineMC is the fork of Purpur compatible with Spigot plugins, offering the best performance for your server."
+  image:
+    src: assets/logo.png
+    alt: DivineMC
+  actions:
+    - theme: brand
+      text: Download
+      link: https://github.com/DivineMC/DivineMC/releases/latest
+    - theme: alt
+      text: Documentation
+      link: /docs/
+---
